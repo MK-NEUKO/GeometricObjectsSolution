527b1f7761331eac8f7c335e4a92ac2d4bf5390b
Michael Koch <m.koch@know-how-akademie.de>
2020-08-12 06:05:24 +0200

    Projektdateien hinzufügen.

---
+++ GeometricObjects/Circle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometricObjects
+{
+    public class Circle
+    {
+        public double XCoordinate;
+        public double YCoordinate;
+        private int _Radius;
+
+        public int Radius
+        {
+            get 
+            { 
+                return _Radius; 
+            }
+            set 
+            {
+                if (value >= 0)
+                {
+                    _Radius = value;
+                }
+                else
+                    Console.WriteLine("Unzulässiger negativer Wert.");
+            }
+        }
+
+    }
+}
